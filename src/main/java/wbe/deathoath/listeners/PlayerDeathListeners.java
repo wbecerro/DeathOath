f4429d239a0f820d4b200b0a204d24fcefa3635d
wbe <34304532+wbecerro@users.noreply.github.com>
2024-10-10 21:47:37 +0200

    Arreglado bug que duplicaba el inventario al perder todas las vidas.

--- src/main/java/wbe/deathoath/listeners/PlayerDeathListeners.java
+++ src/main/java/wbe/deathoath/listeners/PlayerDeathListeners.java
@@ -29,13 +29,6 @@ public class PlayerDeathListeners implements Listener {
 
         if(!player.hasPermission("deathoath.bypass")) {
             utilities.removeLifes(player.getUniqueId(), 1);
-            int lifes = utilities.getLifes(player.getUniqueId());
-
-            if(lifes < 1) {
-                player.ban(DeathOath.messages.outOfLifes, Duration.ofHours(DeathOath.config.hoursOfTimeout),
-                        "WbeIndustries");
-                utilities.setLifes(player.getUniqueId(), DeathOath.config.unbanLifes);
-            }
         } else {
             player.sendMessage(DeathOath.messages.bypass);
         }
--- src/main/java/wbe/deathoath/listeners/PlayerRespawnListeners.java
+++ src/main/java/wbe/deathoath/listeners/PlayerRespawnListeners.java
@@ -6,8 +6,13 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import wbe.deathoath.DeathOath;
+import wbe.deathoath.util.Utilities;
+
+import java.time.Duration;
 
 public class PlayerRespawnListeners implements Listener {
+
+    private Utilities utilities = new Utilities();
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void showDeathMessage(PlayerRespawnEvent event) {
@@ -15,6 +20,14 @@ public class PlayerRespawnListeners implements Listener {
         if(!player.hasPermission("deathoath.bypass")) {
             player.sendTitle(DeathOath.messages.youDiedTitle,
                     DeathOath.messages.youDiedSubTitle, 10, 70, 20);
+
+            int lifes = utilities.getLifes(player.getUniqueId());
+
+            if(lifes < 1) {
+                player.ban(DeathOath.messages.outOfLifes, Duration.ofHours(DeathOath.config.hoursOfTimeout),
+                        "WbeIndustries");
+                utilities.setLifes(player.getUniqueId(), DeathOath.config.unbanLifes);
+            }
         }
     }
 }
